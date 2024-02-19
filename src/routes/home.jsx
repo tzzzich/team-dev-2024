3d7f25297aca124db51631febcd16dc8754132bf
tzzzich <81944587+tzzzich@users.noreply.github.com>
2024-02-20 02:05:25 +0700

    feat(components): add RoleTag to RequestCard comp
    Implemented a componrnt representing the role of a request sender -
    RoleTag and configured it's placement relatively to the RequestCard.
    
    Changed  button icon.
    
    Configured RequestCards' behaviour on the screen.

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -3,11 +3,18 @@ import RequestCard from '/src/components/request-card/RequestCard.jsx';
 
 const Home = () => {
 
+  const requestDataList = Array.from({ length: 10 }, (_, index) => ({
+    date: "30.02.2024",
+    startTime: `10:35 AM`,
+    endTime: `12:10 PM`
+  }));
 
   return (
-
-        <RequestCard  />
-
+    <div className="d-flex flex-row flex-wrap justify-content-space-evenly gap-4 " style={{ width: '80%', margin: '0 auto' }}>
+      {requestDataList.map((index) => (
+        <RequestCard key={index} />
+      ))}
+    </div>
   );
 }
 
