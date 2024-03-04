b5dc91abe7dcb6db7bfe1ba845f45b627e2bb859
Yuriy Eliseev <yurij.eliseev.2004@mail.ru>
2024-03-05 03:00:48 +0700

    Added waiting pages

---
+++ src/routes/VerificationWait.jsx
@@ -0,0 +1,14 @@
+import { Button } from "react-bootstrap"
+
+const VerificationWait = () => {
+    return (
+        <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
+            <img src="/src/assets/Wait.jpg" className="img-fluid"></img>
+            <h1 className="text-center">Верификация в процессе</h1>
+            <h3 className="text-center text-muted">Мы должны удостовериться, что вы являетесь сотрудником деканата</h3>
+            <Button type="button" variant="danger" className="mt-3" size="lg">Выйти</Button>
+        </div>
+    )
+}
+
+export default VerificationWait
---
+++ src/routes/requestPage.jsx
@@ -0,0 +1,28 @@
+import RequestCard from '/src/components/request-page/request-card/RequestCard.jsx';
+import RequestFilter from '/src/components/request-page/RequestFilter.jsx';
+import PaginationNumber from "../components/PaginationNumber.jsx";
+
+const RequestPage = () => {
+
+  const requestDataList = Array.from({ length: 10 }, (_, index) => ({
+    date: "30.02.2024",
+    startTime: `10:35 AM`,
+    endTime: `12:10 PM`
+  }));
+
+  const numbers = [1, 2, 3]
+
+  return (
+    < div style={{ width: '80%', margin: '0 auto' }}>
+      <div className="d-flex flex-row flex-wrap justify-content-space-evenly gap-4 mb-5">
+        <RequestFilter />
+        {requestDataList.map((index) => (
+          <RequestCard key={index} />
+        ))}
+      </div>
+      <PaginationNumber  numbers={numbers} currentPage={2} maxPage={3} />
+    </div>
+  );
+}
+
+export default RequestPage
