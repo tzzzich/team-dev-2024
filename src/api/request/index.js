32d1feb874cadf64acdf97221104ebba367df861
Viktoriya Bukharova <bvv005tkun@gmail.com>
2024-03-11 10:45:30 +0700

    feat(logic): add login, profile, register logic
    Added basic logic for profile, login, registration pages.

--- src/api/request/index.js
+++ src/api/request/index.js
@@ -1,1 +1,6 @@
 export * from "./axiosVerified"
+export * from './axiosKeys'
+export * from './axiosDeleteKey'
+export * from './axiosCreateLock'
+export * from './axiosRegister'
+export * from './axiosPatchProfile'
